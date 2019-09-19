4c4aec7af79ea6a02095999987cbe1e13ceeff02
Alec <a_riddick@fanshaweonline.ca>
2019-09-19 13:52:00 -0400

    Week 3 In Class

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import Person from './modules/DataObject.js';
+import Team from './modules/DataObject.js';
 // Person refers to the object
 
 
@@ -6,7 +6,42 @@ import Person from './modules/DataObject.js';
 // this is the Javascript Module Pattern
 (() => {
 	console.log('My JS issa workin!');
-
-	console.log(Person);
 	
+	let bioData = document.querySelector('.bio-wrapper').children; // grabbing all items within the bio-wrapper div to create an array
+
+	// populate the children within the data object values
+	// thing to the left of the dot is what you're working with
+	// textContent is a property (a physical thing you can change)
+
+	// bioData[0].textContent = Person["name"];
+	// bioData[1].textContent = Person["role"];
+	// bioData[2].textContent = Person["bio"];
+
+	function parsePersonData() {
+		// this = the button/trigger that you clicked on to make the function run
+		let person = this.textContent;
+
+		// populate the children with the data object values
+		bioData[0].src = `images/${Team[person].avatar}`;
+		bioData[1].textContent = Team[person].name;
+		bioData[2].textContent = Team[person].role;
+		bioData[3].textContent = Team[person].bio;
+	}
+
+	// loop through the object and build some UI
+	for (let person in Team) {
+		console.log(person);
+
+		// create a button for each team member
+		let currentButton = document.createElement('button');
+
+		// add the person's name to the button
+		currentButton.textContent = person;
+		// add an event handler to the button
+		currentButton.addEventListener('click', parsePersonData);
+		// put the button in the team section on index.html
+		document.querySelector('.team').appendChild(currentButton);
+
+		// bioData.nextElementSibling.appendChild(currentButton)
+	}
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -1,10 +1,31 @@
 export default {
     // Object or Associative Array
     //  key: value,
+    Trevor: {
+        name: "Trevor",
+        role: "Professor",
+        bio: "Enter the sad story of my life",
+        avatar: "olaf.jpeg"
+    },
 
-    name: "Alec",
-    role: "Student",
-    height: "5ft 11",
-    weight: 185,
-    hairColour: "brown"
+    Justin: {
+        name: "Justin",
+        role: "Professor / Coordinator",
+        bio: "we're not sure, but probably something evil",
+        avatar: "justin.jpeg"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Professor",
+        bio: "I love hunny",
+        avatar: "pooh.jpeg"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Professor",
+        bio: "Constantly needs a weapon",
+        avatar: "pan.jpg"
+    },
 }
